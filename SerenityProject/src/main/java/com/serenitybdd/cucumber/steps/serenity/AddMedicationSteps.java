cd335173d863a24c6bc75aa0b4c74b313e24579b
rsingh <rsingh@EC2AMAZ-GTP2KCB.dfcorp.drfirst.com>
2020-05-30 17:52:14 +0530

    updated steps

--- SerenityProject/src/main/java/com/serenitybdd/cucumber/steps/serenity/AddMedicationSteps.java
+++ SerenityProject/src/main/java/com/serenitybdd/cucumber/steps/serenity/AddMedicationSteps.java
@@ -11,18 +11,21 @@ import net.thucydides.core.annotations.Step;
 public class AddMedicationSteps {
 	Response res = null;
 	
-	 @Step("I submit the Request for {1}")
+	 @Step("Submit Send Patient API as below - {0}.")
 	    public Response submitRequest(String request, String testcaseName){
 		 res = SerenityRest.given().contentType(ContentType.XML).accept(ContentType.XML).body(request).post("http://10.100.12.154:51001/servlet/rcopia.servlet.EngineServlet");
 		 return res;
 	 }
 	 
-	 @Step("Then Response has {0} Code/Text for path {1}")
+	 @Step("Verified {0} is present in response at path {1}")
 	    public void checkResponse(String responseString , String xPathForResponse){
 		 res.then().assertThat().body(xPathForResponse, Matchers.is(responseString));
 	    }
 	 
-	 @Step("DB Validation column value Expected {0} , Actual {1} for DB column name {2} for query {3}")
+	 @Step("Verfied the Database details Expected value {0} for column {2} \n"
+	 		+ "1.Query Ran : {3}"
+			 + "Actual Value:{1}"
+			 + "Expected Value: {0}")
 	    public void checkDBValidation(String expected , String actual, String dbColumnName , String query){
 		 Assert.assertEquals(expected, actual);
 	    }
